a5cb093ed9dbd0d0f1c1e9b51b91fcd29024cac1
Jordan McLaughlin <jmclaughlin1998@gmail.com>
2021-12-01 09:14:13 +0000

    Solution for 01/12/2021

--- First/Program.cs
+++ First/Program.cs
@@ -8,7 +8,40 @@ namespace First
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Initialize Variables
+            var strings = File.ReadAllLines("input.txt");
+            var depths = new int[strings.Count()];
+            
+            for (var i = 0; i < strings.Length; i++)
+            {
+                depths[i] = int.Parse(strings[i]);
+            }
+
+            // Part 1
+            var count = 0;
+            for (int i = 1; i < depths.Length; i++)
+            {
+                if (depths[i] > depths[i - 1])
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Part 1: " + count);
+
+            // Part 2
+            count = 0;
+            var sizeA = 0;
+            var sizeB = 0;
+            for (var i = 3; i < depths.Length; i++)
+            {
+                sizeA = depths[i - 3] + depths[i - 2] + depths[i - 1];
+                sizeB = depths[i - 2] + depths[i - 1] + depths[i];
+                if (sizeB > sizeA)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine("Part 2: " + count);
         }
     }
 }
