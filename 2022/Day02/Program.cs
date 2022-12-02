b3b4e2063ab360d0cea37d064e0afa1c8c99de8e
Jordan McLaughlin <jmcl98@proton.me>
2022-12-02 08:40:24 +0000

    Part 2 + Clean up

--- 2022/Day02/Program.cs
+++ 2022/Day02/Program.cs
@@ -2,84 +2,90 @@
 
 public static class Program
 {
-    // 1,X = Rock. 2,Y = Paper. 3,Z = Scissors.
     public static void Main()
     {
         // Initialize Variables
         var strings = File.ReadAllLines("input.txt");
-        var games = new List<Game>();
+        var games = strings.Select(s => new Game(s)).ToList();
 
-        foreach (var s in strings)
-        {
-            games.Add(new Game(s));
-        }
-
+        // Part 1
         Console.WriteLine("Part 1: {0}", games.Sum(x => x.CalculateScore(1)));
 
+        // Part 2
         Console.WriteLine("Part 2: {0}", games.Sum(x => x.CalculateScore(2)));
-
     }
 }
 
 public class Game
 {
-    public char ElfMove;
-    public char MyMove;
+    private readonly char _elfMove;
+    private readonly char _myMove;
 
     public Game(string moves)
     {
-        ElfMove = moves[0];
-        MyMove = moves[2];
+        _elfMove = moves[0];
+        _myMove = moves[2];
     }
 
     public int CalculateScore(int part)
     {
-        int score;
+        var score = 0;
 
-        if (part == 1)
+        switch (part)
         {
-            switch (MyMove)
-            {
-                case 'X':
-                    score = 1;
-                    score += ElfMove == 'C' ? 6 : 0;
-                    score += ElfMove == 'A' ? 3 : 0;
-                    break;
+            case 1:
+                switch (_myMove)
+                {
+                    case 'X':
+                        score = 1;
+                        score += _elfMove == 'C' ? 6 : 0;
+                        score += _elfMove == 'A' ? 3 : 0;
+                        break;
 
-                case 'Y':
-                    score = 2;
-                    score += ElfMove == 'A' ? 6 : 0;
-                    score += ElfMove == 'B' ? 3 : 0;
-                    break;
+                    case 'Y':
+                        score = 2;
+                        score += _elfMove == 'A' ? 6 : 0;
+                        score += _elfMove == 'B' ? 3 : 0;
+                        break;
 
-                case 'Z':
-                    score = 3;
-                    score += ElfMove == 'B' ? 6 : 0;
-                    score += ElfMove == 'C' ? 3 : 0;
-                    break;
-                
-                default:
-                    score = 0;
-                    break;
+                    case 'Z':
+                        score = 3;
+                        score += _elfMove == 'B' ? 6 : 0;
+                        score += _elfMove == 'C' ? 3 : 0;
+                        break;
+                }
 
-            }
-        }
-        else
-        {
-            switch (MyMove)
-            {
-                case 'X':
-                    score = ElfMove switch
+                break;
+
+            case 2:
+                score = _myMove switch
+                {
+                    'X' => _elfMove switch
                     {
                         'A' => 3,
-                        'B' => 2,
-                        'C' => 3,
+                        'B' => 1,
+                        'C' => 2,
                         _ => 0
-                    };
-            }
-            
-            
-            
+                    },
+                    
+                    'Y' => _elfMove switch
+                    {
+                        'A' => 3 + 1,
+                        'B' => 3 + 2,
+                        'C' => 3 + 3,
+                        _ => 0
+                    },
+                    
+                    'Z' => _elfMove switch
+                    {
+                        'A' => 6 + 2,
+                        'B' => 6 + 3,
+                        'C' => 6 + 1,
+                        _ => 0
+                    }
+                };
+                
+                break;
         }
 
         return score;
